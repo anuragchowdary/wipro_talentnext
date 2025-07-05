c6e513bf55234068d2632347ad871739ca1ff085
chint@LAPTOP-TRRTJH71 <22102a040053@gmail.com>
2025-07-05 15:26:25 +0530

    javafundamentals

--- java_fundamentals/src/java_fundamentals/question_1.java
+++ java_fundamentals/src/java_fundamentals/question_1.java
@@ -1,5 +1,12 @@
 package java_fundamentals;
-
+import java.util.Scanner;
 public class question_1 {
-	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter 1st Name");
+	    String s1=sc.nextLine();
+	    System.out.println("Enter 2nd Name");
+	    String s2=sc.nextLine();
+	    System.out.println(s1+" Technologies "+s2);
+	}
 }
